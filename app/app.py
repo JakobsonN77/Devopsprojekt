d898d531ba648b4273e782905d66f3d851df1de8
JakobsonN77 <116427005+JakobsonN77@users.noreply.github.com>
2025-01-09 18:30:29 +0000

    zmiana programu flask

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, DevOps!"
+@app.route('/', methods=['GET', 'POST'])
+def calculate_square_area():
+    if request.method == 'POST':
+        try:
+            side_length = float(request.form['side_length'])
+            area = side_length ** 2
+            return render_template('result.html', side_length=side_length, area=area)
+        except ValueError:
+            return render_template('index.html', error="Proszę podać poprawną liczbę.")
+    
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
